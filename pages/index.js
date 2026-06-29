7f79ae5f0c775a76dc21f37092247e5ba76d7d89
Ricky2491 <48573902+Ricky2491@users.noreply.github.com>
2026-06-29 16:25:33 -0400

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -211,9 +211,10 @@ export default function Home({ products = [] }) {
   }}
 >
   {/* Icono de Binance en SVG incrustado (no falla) */}
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 1L3.5 9.5L12 18L20.5 9.5L12 1ZM12 14.5L8.5 11L12 7.5L15.5 11L12 14.5ZM12 23L20.5 14.5L18 12L12 18L6 12L3.5 14.5L12 23Z" fill="#805e00"/>
-  </svg>
+  <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <title>Binance</title>
+  <path d="M16.624 13.9202l2.7175 2.7154-7.353 7.353-7.353-7.352 2.7175-2.7164 4.6355 4.6595 4.6356-4.6595zm4.6366-4.6366L24 12l-2.7154 2.7164L18.5682 12l2.6924-2.7164zm-9.272.001l2.7163 2.6914-2.7164 2.7174v-.001L9.2721 12l2.7164-2.7154zm-9.2722-.001L5.4088 12l-2.6914 2.6924L0 12l2.7164-2.7164zM11.9885.0115l7.353 7.329-2.7174 2.7154-4.6356-4.6356-4.6355 4.6595-2.7174-2.7154 7.353-7.353z" fill="#db9900" />
+</svg>
   Pagar con Binance Pay
 </button>
 
